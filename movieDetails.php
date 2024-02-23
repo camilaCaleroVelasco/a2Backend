a4379e59fcd267139c877ff7cbd68ecf2e3bb7f3
Camilacalero <mcc05495@uga.edu>
2024-02-23 14:59:15 -0500

    embed movie

--- movieDetails.php
+++ movieDetails.php
@@ -5,9 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    <iframe width="420" height="315"
-     src="https://www.youtube.com/watch?v=72U5Af8KUpA&t=70s">
-    </iframe>
-</body>
+<html>
+ <body>
+ <iframe width="560" height="315" src="https://www.youtube.com/embed/fFtdbEgnUOk?si=-z3G3s7vWiE3v_uD" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 </html>
