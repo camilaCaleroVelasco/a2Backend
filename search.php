19e9f64d4e3853ead7cc6304ef05d464c4c1d229
Camilacalero <mcc05495@uga.edu>
2024-02-25 15:16:03 -0500

    search images appear

--- search.php
+++ search.php
@@ -5,16 +5,16 @@
         
 
         try {
-            require_once "includes/dbh.inc.php";
+            require_once "includes/databaseConnection.inc.php";
             
-            $query = "SELECT * FROM moviesName WHERE movieName LIKE :moviesearch;"; //sql
+            $query = "SELECT * FROM movies WHERE movie_title LIKE :moviesearch;"; //sql
 
                 $stmt = $pdo->prepare($query);
                 $stmt->bindValue(':moviesearch', '%' . $movieTitle . '%', PDO::PARAM_STR);               
                  $stmt-> execute();
 
                 $results = $stmt->fetchAll(PDO::FETCH_ASSOC); //data from db is in results
-                var_dump($results);
+                #var_dump($results);
 
                 $pdo = null;
                 $stmt = null;
@@ -34,10 +34,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-        <?php include "main.css"?>
-    </style>
-    <title>Document</title>
+    <link href="search.css" rel="stylesheet">
+    <script defer src = "script.js"></script>
+    <title>Search Results</title>
 </head>
 <body>
     <section>
@@ -46,20 +45,26 @@
 
         <?php
 
-            if(empty($results)) {
-                echo "<div>";
-                echo "<p> There were no results! </p>";
-                echo "</div>";
-            }
-            else {
-                foreach ($results as $row) {
-                    echo "<div>";
-                    echo "<h4>" . htmlspecialchars($row["movieName"]) . "</h4>" ;
-                    echo "</div>";
+        if(empty($results)) {
+            echo "<div>";
+            echo "<p> There were no results! </p>";
+            echo "</div>";
+        }
+else {
+    foreach ($results as $row) {
+        echo "<div>";
+    
+                    //<!---Create the image as a button--->
 
+                    echo "<h4>";
+                    echo "<button> <img id='movie' src=" . htmlspecialchars($row["picture"]) . "width='300'
+                    height='400'</h4>" ;
+                    echo "</div>";
                 }
             }
         ?>
+
     </section>
+
 </body>
 </html>
