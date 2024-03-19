85d500a12e66127ac56484f2138b7a666b0fb561
Jeffrey Mendez <75602684+jeffmendezz98@users.noreply.github.com>
2024-03-19 14:28:30 -0400

    Corrections in initialize_db.sql
    Fixed errors in initialize_db.sql

--- DB/initialize_db.sql
+++ DB/initialize_db.sql
@@ -1,19 +1,7 @@
 
-
--- Create User table
-CREATE TABLE User(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    type VARCHAR(255),
-    email VARCHAR(255),
-    password VARCHAR(255),
-    status_id INT,
-    promotion VARCHAR(255),
-    FOREIGN KEY (status_id) REFERENCES Status(status_id)
-);
-
 -- Status enumeration stored in separate table, accessed using primary key
 CREATE TABLE Status(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    status_id INT PRIMARY KEY AUTO_INCREMENT,
     status VARCHAR(255)
 );
 
@@ -36,28 +24,33 @@ VALUES(
 );
 -- End of setting up Status enumeration
     
+-- Create Users table
+CREATE TABLE Users(
+    users_id INT PRIMARY KEY AUTO_INCREMENT,
+    type VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255),
+    status_id INT,
+    FOREIGN KEY (status_id) REFERENCES Status(status_id)
+);
+
 -- Create Admin table, store reference to parent object in User table
 CREATE TABLE Administrator(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT,
+    users_id INT,
     firstName VARCHAR(255),
     lastName VARCHAR(255),
-    FOREIGN KEY (user_id) REFERENCES User(user_id)
+    FOREIGN KEY (users_id) REFERENCES Users(users_id)
 );
 
 -- Create Customer table, store reference to parent object in User table
 CREATE TABLE Customer(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT,
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    users_id INT,
     firstName VARCHAR(255),
     lastName VARCHAR(255),
-    card1_id INT,
-    card2_id INT,
-    card3_id INT,
-    FOREIGN KEY (user_id) REFERENCES User(user_id),
-    FOREIGN KEY (card1_id) REFERENCES PaymentCard(card1_id),
-    FOREIGN KEY (card2_id) REFERENCES PaymentCard(card2_id),
-    FOREIGN KEY (card3_id) REFERENCES PaymentCard(card3_id)
+    numOfCards INT,
+    FOREIGN KEY (users_id) REFERENCES Users(users_id)
 );
 -- INHERITANCE: Child and Parent have separate tables, child table stores key to parent table.
 -- Requires LEFT OUTER JOIN to retrieve all attributes.
@@ -66,9 +59,8 @@ CREATE TABLE Customer(
 -- Parent LEFT OUTER JOIN Child ON
 -- Parent.id = Child.parentid;
 
-
 CREATE TABLE PaymentCard(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    card_id INT PRIMARY KEY AUTO_INCREMENT,
     cardNum INT,
     billingAddress VARCHAR(255),
     expDate VARCHAR(255),
