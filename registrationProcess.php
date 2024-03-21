d779b23d4000a3abdaa6ac4bc272940044cf6dac
Camilacalero <mcc05495@uga.edu>
2024-03-21 17:00:07 -0400

    email duplicate not working

--- registrationProcess.php
+++ registrationProcess.php
@@ -29,7 +29,9 @@
     if ( ! preg_match("/[0-9]/", $_POST["password"])) {
         die("Password must contain at least one number");
     }
+    
     ################ add payment stuff and promo
+
     # check that password and confirmation password
     if ($_POST["password"] !== $_POST["confirm-password"]) {
         die("Confirmation password must match password");
@@ -48,20 +50,28 @@
         die("SQL error: " . $pdo->errorInfo()[2]);
     }
     
-    // Execute the prepared statement with provided parameters
-    if (!$stmt->execute([$_POST["email-address"], $password_hash, $_POST["first-name"], $_POST["last-name"]])) {
-        // If execution fails, display an error message
-        die("Error: Failed to execute the query.");
-    }
+    try {
+        // Execute the prepared statement with provided parameters
+        if (!$stmt->execute([$_POST["email-address"], $password_hash, $_POST["first-name"], $_POST["last-name"]])) {
+            // If execution fails, display an error message
+            die("Error: Failed to execute the query.");
+        }
 
-    // Check if any rows were affected by the insert operation
-    if ($stmt->rowCount() > 0) {
-        // Redirect to the confirmation page if successful
-        header("Location: registrationconfirmation.php");
-        exit;
-    } else {
-        // If no rows were affected, display an error message
-        die("Error: Failed to execute the query.");
+        // Check if any rows were affected by the insert operation
+        if ($stmt->rowCount() > 0) {
+            // Redirect to the confirmation page if successful
+            header("Location: registrationconfirmation.php");
+            exit;
+    }
+    
+    } catch (PDOException $e) {
+
+        //check if email has been used before
+        if ($e->getCode() == 23000 && strpos($e->getMessage(), 'email_UNIQUE') !== false) {
+            die("Email is already taken");
+        } else {
+            die("Error: " . $e->getMessage());
+        }
     }
         
 ?>
