a5df7078946cce929174ff41ad5a6987f55d1709
Camilacalero <mcc05495@uga.edu>
2024-03-26 20:38:45 -0400

    PWD change works
    added comments to the registrationProcess file and also the editProfileProcess

--- registrationProcess.php
+++ registrationProcess.php
@@ -1,5 +1,6 @@
 <?php
     include_once 'includes/databaseConnection.inc.php'; 
+    include_once 'includes/functions.inc.php';
 
     if (empty($_POST["first-name"])) {
         header("Location: register.php?error=firstname");
@@ -36,49 +37,50 @@
         exit();
     }
 
-
-    # check that password and confirmation password--------------------------------------------------
+    // Check that password == confirmation password
     if ($_POST["password"] !== $_POST["confirm-password"]) {
         header("Location: register.php?error=pwdmismatch");
         exit();
     }
     
+    // Hash password
     $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
     
     $pdo = require __DIR__ . "/includes/databaseConnection.inc.php";
 
+    // Create Activation token
     $activation_token = bin2hex(random_bytes(16));
     $activation_token_hash = hash("sha256", $activation_token);
 
 
-    //Check if email already exists--------------------------------------------------
+    //Check if email already exists
     $email = $_POST["email-address"];
-    $sqlCheck = "SELECT COUNT(*) AS count FROM Users WHERE email = ?";
-    $stmtCheck = $pdo->prepare($sqlCheck);
-    $stmtCheck->execute([$email]);
-    $resultCheck = $stmtCheck->fetch(PDO::FETCH_ASSOC);
+    $sqlEmailCheck = "SELECT COUNT(*) AS count FROM Users WHERE email = ?";
+    $stmtEmailCheck = $pdo->prepare($sqlEmailCheck);
+    $stmtEmailCheck->execute([$email]);
+    $resultEmailCheck = $stmtEmailCheck->fetch(PDO::FETCH_ASSOC);
 
-    if ($resultCheck['count'] > 0) {
+    if ($resultEmailCheck['count'] > 0) {
         header("Location: register.php?error=emailexists");
         exit();
     }
 
 
-    // Retrieve $userStatus_id (Active, Inactive)---------------------------------------------------
-    $userStatus = 'Inactive'; //user just created account so active
+    // Retrieve $userStatus_id
+    $userStatus = 'Inactive'; //user just created account so Inactive
     $sqlUserStatus = "SELECT userStatus_id FROM UserStatus WHERE status = ?";
-    $stmtStatus = $pdo->prepare($sqlUserStatus);
-    $stmtStatus->execute([$userStatus]);
-    $userStatusRow = $stmtStatus->fetch(PDO::FETCH_ASSOC);
+    $stmtUserStatus = $pdo->prepare($sqlUserStatus);
+    $stmtUserStatus->execute([$userStatus]);
+    $userStatusRow = $stmtUserStatus->fetch(PDO::FETCH_ASSOC);
     if ($userStatusRow) {
         $userStatus_id = $userStatusRow['userStatus_id'];
     } else {
-
         die("UserStatus not found for status: $userStatus");
+        exit();
     }
 
-    // Retrieve $userType_id (Customer, Admin)--------------------------------------------------
-    $userType = 'Customer'; //default user creation has to be customer
+    // Retrieve $userType_id
+    $userType = 'Customer'; // Default user creation has to be customer
     $sqlUserType = "SELECT userType_id FROM UserType WHERE type = ?";
     $stmtUserType = $pdo->prepare($sqlUserType);
     $stmtUserType->execute([$userType]);
@@ -89,16 +91,13 @@
         die("UserType not found for type: $userType");
     }
 
-    //Phone Number---------------------------------------------------------------------------
-    $phoneNumber = $_POST["phone-number"]; //retieve from POST
+    // Retrieve Phone Number
+    $phoneNumber = $_POST["phone-number"];
 
 
-    // Retrieve $cardType_id based on card type (Visa, MasterCard, AmericanExpress)-------------------------
+    // Retrieve $cardType_id 
     $cardType = $_POST["card-type"];
-
-    // Default card type in case the specified card type doesn't exist
-    $defaultCardType = 'Visa';
-
+    $defaultCardType = 'Visa'; // Default cardType
     $sqlCardType = "SELECT cardType_id FROM PaymentCardType WHERE type = ?";
     $stmtCardType = $pdo->prepare($sqlCardType);
     $stmtCardType->execute([$cardType]);
@@ -106,7 +105,7 @@
     if ($cardTypeRow) {
         $cardType_id = $cardTypeRow['cardType_id'];
     } else {
-        // Use default card type if the specified card type doesn't exist
+        // If user does not provide card type assign the default card type
         $stmtDefaultCardType = $pdo->prepare($sqlCardType);
         $stmtDefaultCardType->execute([$defaultCardType]);
         $defaultCardTypeRow = $stmtDefaultCardType->fetch(PDO::FETCH_ASSOC);
@@ -114,13 +113,9 @@
         if ($defaultCardTypeRow) {
             $cardType_id = $defaultCardTypeRow['cardType_id'];
         } 
-        // Removed to allow users to input blank card
-        //else {
-        //    die("Default CardType not found: $defaultCardType");
-        //} 
     }
 
-    // Encrypt the credit card number--------------------------------------------------
+    // Encrypt card number
     $encryptedCardNumber = null;
     if (!empty($_POST["card-number"])) {
         $cardNumber = $_POST["card-number"];
@@ -128,30 +123,33 @@
         $encryptedCardNumber = openssl_encrypt($cardNumber, 'aes-256-cbc', $encryptionKey, 0, $encryptionKey);
     }
 
-    // Insert into BillingAddress table if provided--------------------------------------------------
+    // Insert Billing Address info if provided
     $billingAddressId = null;
-    if (!empty($_POST["street-address-billing"]) && !empty($_POST["city-billing"]) && !empty($_POST["state-billing"]) && !empty($_POST["zip-code-billing"])) {
+    if (!empty($_POST["street-address-billing"]) && !empty($_POST["city-billing"]) && 
+        !empty($_POST["state-billing"]) && !empty($_POST["zip-code-billing"])) {
+            
         $sqlInsertBillingAddress = "INSERT INTO BillingAddress (billingStreetAddress, billingCity, billingState, billingZipCode) VALUES (?, ?, ?, ?)";
         $stmtInsertBillingAddress = $pdo->prepare($sqlInsertBillingAddress);
         if (!$stmtInsertBillingAddress) {
             die("SQL error: " . $pdo->errorInfo()[2]);
         }
 
-        // Execute the query and handle errors
+        // Handle errors
         try {
+            // Execute
             $successInsertBillingAddress = $stmtInsertBillingAddress->execute([$_POST["street-address-billing"], $_POST["city-billing"], $_POST["state-billing"], $_POST["zip-code-billing"]]);
 
             if (!$successInsertBillingAddress) {
                 die("Error: Failed to insert billing address. " . implode(", ", $stmtInsertBillingAddress->errorInfo()));
             }
 
-            // Retrieve the auto-generated billing_id
+            // Retrieve billing_id
             $billingAddressId = $pdo->lastInsertId();
         } catch (PDOException $e) {
             die("Error: " . $e->getMessage());
         }
     } else {
-        // If delivery address is not provided, assign a default billing_id
+        // Track billing_id even if the user did not provide Billing Address
         $sqlDefaultBilling = "INSERT INTO BillingAddress (billingStreetAddress, billingCity, billingState, billingZipCode) VALUES (?, ?, ?, ?)";
         $stmtDefaultBilling = $pdo->prepare($sqlDefaultBilling);
         if (!$stmtDefaultBilling) {
@@ -164,44 +162,51 @@
         $defaultBillingState = "";
         $defaultBillingZipCode = "0"; //change to varchar in DB
 
+        // Handle errors
         try {
+            // Execute
             $successDefaultBilling= $stmtDefaultBilling->execute([$defaultBillingStreet, $defaultBillingCity, $defaultBillingState, $defaultBillingZipCode]);
 
             if (!$successDefaultBilling) {
                 die("Error: Failed to assign default delivery address. " . implode(", ", $stmtDefaultBilling->errorInfo()));
             }
 
-            // Retrieve the auto-generated billing ID
+            // Retrieve billing_id
             $billingAddressId = $pdo->lastInsertId();
+
         } catch (PDOException $e) {
             die("Error: " . $e->getMessage());
         }
     }
 
-    // Insert into DeliveryAddress table if provided--------------------------------------------------
+    // Insert Delivery Address info if provided
     $deliveryAddressId = null;
-    if (!empty($_POST["street-address-shipping"]) && !empty($_POST["city-shipping"]) && !empty($_POST["state-shipping"]) && !empty($_POST["zip-code-shipping"])) {
+    if (!empty($_POST["street-address-shipping"]) && !empty($_POST["city-shipping"]) && 
+        !empty($_POST["state-shipping"]) && !empty($_POST["zip-code-shipping"])) {
+
         $sqlInsertDeliveryAddress = "INSERT INTO DeliveryAddress (deliveryStreetAddress, deliveryCity, deliveryState, deliveryZipCode) VALUES (?, ?, ?, ?)";
         $stmtInsertDeliveryAddress = $pdo->prepare($sqlInsertDeliveryAddress);
         if (!$stmtInsertDeliveryAddress) {
             die("SQL error: " . $pdo->errorInfo()[2]);
         }
 
-        // Execute the query and handle errors
+        // Handle errors
         try {
+            // Execute
             $successInsertDeliveryAddress = $stmtInsertDeliveryAddress->execute([$_POST["street-address-shipping"], $_POST["city-shipping"], $_POST["state-shipping"], $_POST["zip-code-shipping"]]);
 
             if (!$successInsertDeliveryAddress) {
                 die("Error: Failed to insert delivery address. " . implode(", ", $stmtInsertDeliveryAddress->errorInfo()));
             }
 
-            // Retrieve the auto-generated delivery ID
+            // Retrieve delivery_id
             $deliveryAddressId = $pdo->lastInsertId();
+
         } catch (PDOException $e) {
             die("Error: " . $e->getMessage());
         }
     } else {
-        // If delivery address is not provided, assign a default delivery_id
+        // Track delivery_id even if the user did not provide Delivery Address
         $sqlDefaultDelivery = "INSERT INTO DeliveryAddress (deliveryStreetAddress, deliveryCity, deliveryState, deliveryZipCode) VALUES (?, ?, ?, ?)";
         $stmtDefaultDelivery = $pdo->prepare($sqlDefaultDelivery);
         if (!$stmtDefaultDelivery) {
@@ -214,27 +219,28 @@
         $defaultState = "";
         $defaultZipCode = "0"; //change to varchar in DB
 
+        // Handle errors
         try {
+            // Execute
             $successDefaultDelivery = $stmtDefaultDelivery->execute([$defaultStreet, $defaultCity, $defaultState, $defaultZipCode]);
 
             if (!$successDefaultDelivery) {
                 die("Error: Failed to assign default delivery address. " . implode(", ", $stmtDefaultDelivery->errorInfo()));
             }
 
-            // Retrieve the auto-generated delivery ID
+            // Retrieve delivery_id
             $deliveryAddressId = $pdo->lastInsertId();
         } catch (PDOException $e) {
             die("Error: " . $e->getMessage());
         }
     }
 
-    //Promotion---------------------------------------------------------------------------
-    // Determine promo subscription based on checkbox status
+    // Promo Subscription 
     $promoSubscription = isset($_POST['subscribe-promos']) ? 1 : 2;
 
 
 
-    // Insert into Users table---------------------------------------------------
+    // Insert into Users table
     $sqlUsers = "INSERT INTO Users (email, password, firstName, lastName, phoneNumber, numOfCards, account_activation_hash, userStatus_id, userType_id, promoSub_id, billing_id, delivery_id)
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     $stmtUsers = $pdo->prepare($sqlUsers);
@@ -242,29 +248,31 @@
         die("SQL error: " . $pdo->errorInfo()[2]);
     }
 
-    //Number of cards added---------------------------------------------------------------------------
+    //Number of cards added
     $numOfCards = 0; // Default value
     $lastUserId = null;
     if (!empty($encryptedCardNumber) && !empty($_POST["expiration-month"]) && !empty($_POST["expiration-year"])) {
-        // Check if user has less than 3 cards before incrementing...
+        // Check if the user has less than 3 cards
         $sqlNumOfCards = "SELECT COUNT(*) AS cardCount FROM PaymentCard WHERE users_id = ?";
         $stmtNumOfCards = $pdo->prepare($sqlNumOfCards);
         $stmtNumOfCards->execute([$lastUserId]);
         $cardCountRow = $stmtNumOfCards->fetch(PDO::FETCH_ASSOC);
         $numOfCards = $cardCountRow['cardCount'] < 3 ? $cardCountRow['cardCount'] + 1 : 3;
     }
-    // Users execute ---------------------------------------------------------------------------
+
+    // Users execute 
     if (!$stmtUsers->execute([$_POST["email-address"], $password_hash, $_POST["first-name"], $_POST["last-name"], $phoneNumber, $numOfCards, $activation_token_hash, $userStatus_id, $userType_id, $promoSubscription, $billingAddressId, $deliveryAddressId])) {
         die("Error: Failed to execute the Users query.");
     }
 
 
-    // Check if any rows were affected by the insert operation
+    // Check if the rows are affected
     if ($stmtUsers->rowCount() > 0) {
-        // Retrieve the last inserted user ID
+
+        // Retrieve the last inserted user_id
         $lastUserId = $pdo->lastInsertId();
 
-        // Check if payment card information is provided and insert into PaymentCard table
+        // Check if payment card was added
         if (!empty($encryptedCardNumber) && !empty($_POST["expiration-month"]) && !empty($_POST["expiration-year"])) {
             $sqlPaymentCard = "INSERT INTO PaymentCard (cardNum, cardType_id, expMonth, expYear, firstName, lastName, users_id)
                             VALUES (?, ?, ?, ?, ?, ?, ?)";
@@ -277,18 +285,19 @@
             }
         }
 
-        // Email Verification Process---------------------------------------------------
+        // Email Verification Process
+        $name = $_POST["first-name"];
         $path = (@$_SERVER["HTTPS"] == "on") ? "https://" : "http://";
         $path .=$_SERVER["SERVER_NAME"]. dirname($_SERVER["PHP_SELF"]);        
-        //echo $path;
         $mail = require __DIR__ . "/mailer.php";
         $mail->setFrom("noreply@example.com");
         $mail->addAddress($_POST["email-address"]);
         $mail->Subject = "Account Activation";
         $mail->Body = <<<END
+        Welcome $name <br>
         Click <a href="$path/activateAccount.php?token=$activation_token">here</a> 
         to activate your account. 
-        END; // Change URL according to your localhost directory
+        END; 
         try {
             $mail->send();
         } catch (Exception $e) {
@@ -296,9 +305,10 @@
             exit;
         }
 
+        
 
-        // Redirect to the confirmation page if successful
-        header("Location: registrationconfirmation.php");
+        // Redirect 
+        header("Location: registrationconfirmation.php?success=accountCreated");
         exit;
     } 
     else {
