c7a644164b2ad975cbefa8e1dd0bf6209a9d251f
tairaakiolu <taira.akiolu@uga.edu>
2024-03-27 01:16:23 -0400

    removed some pictures and updated edit profile

---
+++ restrictedAccess.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA Compatible" content="IE=edge">
+    <title>A2 MOVIES</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Enable to put style on page -->
+     <link rel="stylesheet" href="css/login.css">
+     <link rel="stylesheet" href="css/restrictedAccess.css">
+    
+     
+</head>
+<body>
+
+    <header>
+        <a href= "index.php">
+            <img class="logo" src="images/A2 movies Icon.jpeg" alt="logo">
+        </a>
+    </header>
+
+         <div class ="wrapper">   <!--Div class for wrapper starts here -->
+
+            <svg  class = "lock-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width = "200"  ><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+            <path fill="#808080" d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z"/>
+           </svg>
+         
+
+
+      
+            <h1> You do not have permission to access this page!</h1>
+            <p>Please go back to the previous page or log in as a different user.</p>
+            <!-- Button Box for log in page -->
+            <div class = "button-box">
+                <button type = "submit" class ="button" name="access"> <a href ="login.php" class = button-link> Login as different user </a> </button>
+            </div>
+            
+         </div> <!--Div class for wrapper ends  here -->
+
+         
+
+</body>
+</html>
+
+
