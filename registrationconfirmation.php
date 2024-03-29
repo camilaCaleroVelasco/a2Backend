4551251eb85f09a3c7fa8cb445e5091052ac1015
tairaakiolu <taira.akiolu@uga.edu>
2024-03-29 10:29:35 -0400

    added background picture

--- registrationconfirmation.php
+++ registrationconfirmation.php
@@ -8,8 +8,14 @@
     <title>A2 MOVIES</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/main.css">
+  
+
+    <link rel="stylesheet" href="css/login.css">
+    
     <link rel="stylesheet" href="css/registrationconfirmation.css">
+ 
+   
+    
    
 </head>
 
