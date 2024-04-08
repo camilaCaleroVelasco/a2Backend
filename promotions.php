777ff7badc4bf0dedf6fc8cd7937e30fdda8ee9d
tairaakiolu <taira.akiolu@uga.edu>
2024-04-08 17:08:44 -0400

    updated promotions and small fixes on search page

--- promotions.php
+++ promotions.php
@@ -1,80 +1,152 @@
 <?php
-    session_start();
-    if ((!isset($_SESSION["email"])) || (isset($_SESSION["email"]) && $_SESSION["userType_id"] !== 2)) {
-    
-        header("Location: restrictedAccess.php");
-        exit();
-    }
+session_start();
+if ((!isset($_SESSION["email"])) || (isset($_SESSION["email"]) && $_SESSION["userType_id"] !== 2)) {
+
+    header("Location: restrictedAccess.php");
+    exit();
+}
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA Compatible" content="IE=edge">
     <title>A2 MOVIES</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/promotions.css">
+
+    <!-- Enable back on to put style -->
+    <link rel="stylesheet" href="css/register.css">
+    <link rel="stylesheet" href="css/promotions.css"> 
 
 </head>
 
 <body>
+    <header>
+        <a href="index.php">
+            <img class="logo" src="images/A2 Movies Icon.jpeg" alt="logo">
+            <!--clicking a2 movies icon will link back to homepage-->
+        </a>
+    </header>
 
-<header>
-    <a href= "index.php">
-        <img class="logo" src="images/A2 Movies Icon.jpeg" alt="logo">  <!--clicking a2 movies icon will link back to homepage-->
-    </a>
-</header>
+    <!-- Beginning Field for Register Info -->
+    <div class="wrapper"> <!-- div class wrapper starts here -->
+        <h1> Promotions </h1>
 
-<div class ="tabs">   <!-- div class tabs starts here-->
-    <div class="tabs-sidebar">
-        <button class= "tabs-button tabs-button-active" tab-info = "1" > View Promotions </button>
-        <button class= "tabs-button " tab-info = "2" > Add Promotions </button>
-        <button class= "tabs-button " tab-info = "3" > Edit Promotions </button>
-    </div>
-    
-    <div class = "tabs-content tabs-content-active" tab-info-data = "1" >
-        <h1> View Promotions</h1>
-        <div id ="view-promotions" class ="view-promotions-container">
-            <!-- Promotions will appear with a delete button-->
-        </div>
-    </div>
-    
-    <div class = "tabs-content" tab-info-data = "2" >
-        <h1> Add Promotions</h1>
-        <form id = "promotion-form">
-            <div class = "form-input">
-                <label for="movie">Movie:</label>
-                <input type="text" id="movie" name="movie" placeholder="Enter movie title">
+        <h2> Promotion Content</h2>
+
+
+        <form action="#" method="post"> <!--push info to the DB -->
+
+
+            <div class="input-box"> <!-- div class input-box starts here -->
+
+                <div class="field">
+                    <input type="text" placeholder="Promotion Name" name="promotion-name" required>
+                    <!-- user icon from fontawesome -->
+                    <svg id="user-icon1" class="icon" xmlns="http://www.w3.org/2000/svg" width="30" height="20"
+                        fill="#FFFFFF"
+                        viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                        <path
+                            d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
+                    </svg>
+                </div>
+
+                <div class="field">
+                    <input type="number" placeholder=" Promotion Amount (%)" name="promotion-amount" required>
+                    <!-- percent icon from fontawesome -->
+                    <svg id="user-icon1" class="icon" xmlns="http://www.w3.org/2000/svg" width="30" height="20"
+                        fill="#FFFFFF"
+                        viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                        <path
+                            d="M374.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-320 320c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l320-320zM128 128A64 64 0 1 0 0 128a64 64 0 1 0 128 0zM384 384a64 64 0 1 0 -128 0 64 64 0 1 0 128 0z" />
+                    </svg>
+                </div>
+
+                <div class="field">
+                    <input type="number" placeholder="Promotion Code" name="promotion-code" required>
+                    <!-- user icon from fontawesome -->
+                    <svg id="mail-icon" class="icon" xmlns="http://www.w3.org/2000/svg" width="30" height="20"
+                        fill="#FFFFFF"
+                        viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                        <path
+                            d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
+                    </svg>
+                </div>
+
+
+
+            </div> <!-- div class input-box ends here -->
+
+
+            <h3> Promotion Date</h3>
+
+      
+
+
+            <div class=" input-box date-information"> <!-- Card Info Fields start here-->
+
+                <div class="field">
+                    <input type="number" placeholder="Start Month" name="start-month" required>
+                    <!-- user icon from fontawesome -->
+                    <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="30" height="20" fill="#FFFFFF"
+                        viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                        <path
+                            d="M128 0c17.7 0 32 14.3 32 32V64H288V32c0-17.7 14.3-32 32-32s32 14.3 32 32V64h48c26.5 0 48 21.5 48 48v48H0V112C0 85.5 21.5 64 48 64H96V32c0-17.7 14.3-32 32-32zM0 192H448V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V192zm64 80v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm128 0v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H336zM64 400v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H208zm112 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H336c-8.8 0-16 7.2-16 16z" />
+                    </svg>
+
+                </div>
+
+                <div class="field">
+                    <input type="number" placeholder="Start Day" name="start-day" required>
+                    <!-- user icon from fontawesome -->
+                    <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="30" height="20" fill="#FFFFFF"
+                        viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                        <path
+                            d="M128 0c17.7 0 32 14.3 32 32V64H288V32c0-17.7 14.3-32 32-32s32 14.3 32 32V64h48c26.5 0 48 21.5 48 48v48H0V112C0 85.5 21.5 64 48 64H96V32c0-17.7 14.3-32 32-32zM0 192H448V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V192zm64 80v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm128 0v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H336zM64 400v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H208zm112 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H336c-8.8 0-16 7.2-16 16z" />
+                    </svg>
+                </div>
+
+                <div class="field">
+                    <input type="number" placeholder="End Month" name="end-month" required>
+                    <!-- user icon from fontawesome -->
+                    <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="30" height="20" fill="#FFFFFF"
+                        viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                        <path
+                            d="M128 0c17.7 0 32 14.3 32 32V64H288V32c0-17.7 14.3-32 32-32s32 14.3 32 32V64h48c26.5 0 48 21.5 48 48v48H0V112C0 85.5 21.5 64 48 64H96V32c0-17.7 14.3-32 32-32zM0 192H448V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V192zm64 80v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm128 0v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H336zM64 400v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H208zm112 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H336c-8.8 0-16 7.2-16 16z" />
+                    </svg>
+                </div>
+
+
+                <div class="field">
+                    <input type="number" placeholder="End Day" name="end-day" required>
+                    <!-- user icon from fontawesome -->
+                    <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="30" height="20" fill="#FFFFFF"
+                        viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                        <path
+                            d="M128 0c17.7 0 32 14.3 32 32V64H288V32c0-17.7 14.3-32 32-32s32 14.3 32 32V64h48c26.5 0 48 21.5 48 48v48H0V112C0 85.5 21.5 64 48 64H96V32c0-17.7 14.3-32 32-32zM0 192H448V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V192zm64 80v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm128 0v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H336zM64 400v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H208zm112 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H336c-8.8 0-16 7.2-16 16z" />
+                    </svg>
+                </div>
+
+
+            </div> 
+
+            <div class="button-container">
+
+                <button type="submit" name=add-promotion-button; class="button" id="submit">
+                    Add Promotion </button>
+                <a href="currentPromotions.php" class="button" id="submit">View Current Promotions</a>
+
             </div>
-            
-            <div class ="form-input">
-                <label for="discount-name">Content:</label>
-                <input type="text" id="discount-name" name="discount-name" placeholder="Enter Promotions name">
-            </div>
-            
-            <div class = "form-input"> 
-                <label for="discount-amount">Amount:</label>
-                <input type="text" id="discount-amount" name="discount-amount" placeholder="Enter Promotions amount">
-            </div>
-            <button type="submit" class = "add-promotion-button">Add Promotion</button>
         </form>
-    
-    </div>
 
 
-    <div class = "tabs-content" tab-info-data = "3" >
-        <h1> Edit Promotions</h1>
-        <div id ="edit-promotions-content" class = "edit-promotions-container">
-            <!-- Promotions will appear with a delete button-->
-        </div>
-    </div>
-</div>  <!-- div class tabs end here-->
 
-<script src="javascript/promotions.js"></script>
+    </div> <!-- div class wrapper ends here -->
 
 
 </body>
+
 </html>
