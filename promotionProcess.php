9c5c447c6e187a36a9bc7e662b159be086c09db4
Camilacalero <mcc05495@uga.edu>
2024-04-09 20:30:08 -0400

    fixed small bug

--- promotionProcess.php
+++ promotionProcess.php
@@ -32,20 +32,23 @@ ini_set('display_errors', 1);
         die("Promotion Status not found: $promoStatus");
     }
 
-    // Set variables 
+    // Set variable
     $promoStatusId = $promoStatusRow['promoStatus_id'];
-    $promoName = $_POST["promotion-name"];
-    $promoAmount = $_POST["promotion-amount"];
-    $promoCode = $_POST["promotion-code"];
-    $promoStartMonth = $_POST["start-month"];
-    $promoStartDay = $_POST["start-day"];
-    $promoEndMonth = $_POST["end-month"];
-    $promoEndDay = $_POST["end-day"];
+    
 
     // Check if the add promotion button was clicked and if all fields are not empty
-    if (isset($_POST["add-promotion-button"]) && !empty($promoName) && !empty($promoAmount) && 
-    !empty($promoCode) && !empty($promoStartMonth) && !empty($promoStartDay) && 
-    !empty($promoEndMonth) && !empty($promoEndDay)){
+    if (isset($_POST["add-promotion-button"]) && !empty($_POST["promotion-name"]) && !empty($_POST["promotion-amount"]) && 
+    !empty($_POST["promotion-code"]) && !empty($_POST["start-month"]) && !empty($_POST["start-day"]) && 
+    !empty($_POST["end-month"]) && !empty($_POST["end-day"])){
+
+        // Set variables
+        $promoName = $_POST["promotion-name"];
+        $promoAmount = $_POST["promotion-amount"];
+        $promoCode = $_POST["promotion-code"];
+        $promoStartMonth = $_POST["start-month"];
+        $promoStartDay = $_POST["start-day"];
+        $promoEndMonth = $_POST["end-month"];
+        $promoEndDay = $_POST["end-day"];
 
             // SQL to insert info into DB
             $sqlInsertPromo = "INSERT INTO promotion (promoName, promoCode, startDay, startMonth, endDay, endMonth, promoStatus_id ,percentDiscount)
