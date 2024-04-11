7b70acee57766f478278f44472bdbe8a16a14744
Camilacalero <mcc05495@uga.edu>
2024-04-11 16:12:43 -0400

    adminMovie separated

---
+++ Get/adminMovieGet.php
@@ -0,0 +1,18 @@
+<?php
+
+session_start();
+require_once "includes/dbh.inc.php";
+require_once "functions/checkIfAdminFunction.php"; 
+
+function adminMovieGet($conn, $sql) {
+    $sqlNP = "SELECT * FROM movies WHERE movie_status LIKE '%now playing%'";
+    $sqlCS = "SELECT * FROM movies WHERE movie_status LIKE '%coming soon%'";
+    $resultNP = mysqli_query( $conn, $sqlNP ) or die("bad query: $sql");
+    $resultCS = mysqli_query( $conn, $sqlCS ) or die("bad query: $sql");
+
+    return [
+        'nowPlaying' => $resultNP,
+        'comingSoon' => $resultCS
+    ];
+}
+        
