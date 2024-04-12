60f711b17c2e363cd2b8349067f22dd78abad0fc
tairaakiolu <taira.akiolu@uga.edu>
2024-04-11 21:46:57 -0400

    fixed select drop down box in one card sec

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -513,7 +513,7 @@ function userExists($conn, $email) {
     }
     // Delete
     function deleteUserPaymentCard1($conn, $usersid) {
-        $sql = "UPDATE paymentcard1 SET cardNum = '', cardType_id = 1, expMonth = '', expYear = '',
+        $sql = "UPDATE paymentcard1 SET cardNum = '', cardType_id = NULL, expMonth = '', expYear = '',
         firstName = '', lastName = '' WHERE users_id = ?;";
         $stmt = mysqli_stmt_init($conn);
         if(!mysqli_stmt_prepare($stmt, $sql)) {
@@ -524,6 +524,8 @@ function userExists($conn, $email) {
         mysqli_stmt_bind_param($stmt, "i", $usersid);
         mysqli_stmt_execute($stmt);
         mysqli_stmt_close($stmt);
+         
+
     }
 
     function deleteUserPaymentCard2($conn, $usersid) {
