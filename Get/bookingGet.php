c289a4b7b733bc74763d458f5a5e1f165eb4cf8b
Camilacalero <mcc05495@uga.edu>
2024-04-25 15:17:08 -0400

    moved ageSelect php code to ageSelectGet

--- Get/bookingGet.php
+++ Get/bookingGet.php
@@ -1,38 +1,36 @@
 
 <?php
     function getMovieID($conn) {
-            if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["movie_id"])) {
+        if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["movie_id"])) {
 
-                require_once "includes/dbh.inc.php";
+            require_once "includes/dbh.inc.php";
+        
+            $movie_id = $_GET["movie_id"];
             
-                $movie_id = $_GET["movie_id"];
-                $dates = getDates($conn, $movie_id);
-                $times = getTimes($conn, $movie_id);
+            $sql = "SELECT * FROM movies WHERE movie_id = ?";
+        
             
-                $sql = "SELECT * FROM movies WHERE movie_id = ?";
-            
-                
-                $stmt = mysqli_stmt_init($conn);
-            
-                if(!mysqli_stmt_prepare($stmt, $sql)) {
-                    header("Location: booking.php?error=stmtfailed"); 
-                    exit();
-                }
-            
-                mysqli_stmt_bind_param($stmt, "i", $movie_id);
-                mysqli_stmt_execute($stmt);
-            
-                $resultData = mysqli_stmt_get_result($stmt); // Fetch single movie
-            
-                
-                $movie = mysqli_fetch_assoc($resultData);
-            
-                if (!$movie) { // Check if movie found
-                    echo "<p>Movie not found.</p>";
-                }
-            
-            } else {
-                header("Location: index.php"); // Redirect if no movie ID
+            $stmt = mysqli_stmt_init($conn);
+        
+            if(!mysqli_stmt_prepare($stmt, $sql)) {
+                header("Location: booking.php?error=stmtfailed"); 
+                exit();
             }
-            return $movie;
+        
+            mysqli_stmt_bind_param($stmt, "i", $movie_id);
+            mysqli_stmt_execute($stmt);
+        
+            $resultData = mysqli_stmt_get_result($stmt); // Fetch single movie
+        
+            
+            $movie = mysqli_fetch_assoc($resultData);
+        
+            if (!$movie) { // Check if movie found
+                echo "<p>Movie not found.</p>";
+            }
+        
+        } else {
+            header("Location: index.php"); // Redirect if no movie ID
         }
+        return $movie;
+    }
--- ageselect.php
+++ ageselect.php
@@ -1,35 +1,9 @@
 <?php
 session_start();
-if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["movie_id"]) && isset($_SESSION["email"])) {
+require_once "Get/ageSelectGet.php"; 
+$movie = getMovieID($conn);
+$movie_id = $movie["movie_id"]; 
 
-    $movie_id = $_GET["movie_id"];
-        require_once "includes/dbh.inc.php";
-
-        $sql = "SELECT * FROM movies WHERE movie_id = ?";
-
-        
-        $stmt = mysqli_stmt_init($conn);
-
-        if(!mysqli_stmt_prepare($stmt, $sql)) {
-            header("Location: booking.php?error=stmtfailed"); 
-            exit();
-        }
-
-        mysqli_stmt_bind_param($stmt, "i", $movie_id);
-        mysqli_stmt_execute($stmt);
-
-        $resultData = mysqli_stmt_get_result($stmt); // Fetch single movie
-
-        
-        $movie = mysqli_fetch_assoc($resultData);
-
-        if (!$movie) { // Check if movie found
-            echo "<p>Movie not found.</p>";
-        }
-
-} else {
-    header("Location: index.php"); // Redirect if no movie ID
-}
 ?>
 
 <!DOCTYPE html>
