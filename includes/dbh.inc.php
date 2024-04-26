db55baf9b4107180088cb0abad120e239d5393a5
Camilacalero <mcc05495@uga.edu>
2024-04-26 10:43:46 -0400

    added aws info for connecting

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -5,6 +5,11 @@ $dBUsername = "root";
 $dBPassword = "";
 $dBName = "movies";
 
+// $servername = "movies.c1escyq2w908.us-east-1.rds.amazonaws.com";
+// $dBUsername = "admin";
+// $dBPassword = "passworda2movies";
+// $dBName = "movies";
+
 $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 
 if (!$conn) {
@@ -12,3 +17,4 @@ if (!$conn) {
 }
 
 return $conn;
+
